dde935960dd74e358585fb943156791a91b461e7
Tobias Matthäus Gobbo Scapin <TMGS@dd.homenet.telecomitalia.it>
2020-06-09 16:15:27 +0200

    Menu

--- src/components/components.jsx
+++ src/components/components.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export function HomeText({ children }) {
 	return (
 		<div className="vh-100 vh-90-m flex bg-purple">
@@ -13,11 +14,11 @@ export function HomeText({ children }) {
 export function HomeProject(props) {
 	return (
 		<section className="ma3 flex">
-			<div className=" w-30 mr3 h6 flex flex-column justify-center items-end">
-				<div className="helvetica fw7 f1 pb3">{props.title}</div>
-				<div className="minion f3 orange">{props.subtitle}</div>
+			<div className=" w-30 mr3 h6 h5-m flex flex-column justify-center items-end tr">
+				<div className="helvetica fw7 f3-m f1 pb3 pb2-m">{props.title}</div>
+				<div className="minion f3 f4-m orange">{props.subtitle}</div>
 			</div>
-			<div className="w-70 h6 bg-green" />
+			<div className="w-70 h6 h5-m bg-green" />
 		</section>
 	);
 }
